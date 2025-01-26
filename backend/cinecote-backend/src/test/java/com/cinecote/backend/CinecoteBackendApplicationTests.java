99977c387fadaff9f75aea4c494a2a46ee77f369
Abraham Arce <aarce104@gmail.com>
2025-01-26 02:33:34 -0500

    First commit.

---
+++ backend/cinecote-backend/src/test/java/com/cinecote/backend/CinecoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinecote.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinecoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
